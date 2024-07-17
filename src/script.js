b01f773af67e1848339273114951427a73275335
Misato Tanno <breitmeer.0804@gmail.com>
2024-07-17 17:17:15 -0600

    Added animation

--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three'
+import gsap from 'gsap'
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -6,93 +7,71 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
-/**
- * Objects
- */
-const group = new THREE.Group()
-scene.add(group)
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
 
-const cube1 = new THREE.Mesh( 
-    new THREE.BoxGeometry(1,1,1),
-    new THREE.MeshBasicMaterial({color: 0xff0000})
-)
-
-const cube2 = new THREE.Mesh( 
-    new THREE.BoxGeometry(1,1,1),
-    new THREE.MeshBasicMaterial({color: 0x00ff00})
-)
-
-cube2.position.x = - 2
-
-const cube3 = new THREE.Mesh( 
-    new THREE.BoxGeometry(1,1,1),
-    new THREE.MeshBasicMaterial({color: 0x0000ff})
-)
-
-cube3.position.x =  2
-
-group.add(cube1)
-group.add(cube2)
-group.add(cube3)
-/**
- * Object
- */
-// const geometry = new THREE.BoxGeometry(1, 1, 1)
-// const material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true })
-// const mesh = new THREE.Mesh(geometry, material)
-
-// 1 can be anything.
-// mesh.position.set(.7, -.6, 1)
-
-
-
-/**
- * Axes Helper
- */
-
-const axes = new THREE.AxesHelper()
-scene.add(axes)
-
-/**
- * SCALE OBJECT
- */
-// mesh.scale.set(2, .5, .5)
-
-/**
- * ROTATE OBJECT
- */
-// mesh.rotation.reorder("YXZ")
-// mesh.rotation.y = Math.PI * .25
-// mesh.rotation.x = Math.PI * .25
-
-
-/**
- * Sizes
- */
+// Sizes
 const sizes = {
     width: 800,
     height: 600
 }
 
-/**
- * Camera
- */
+// Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
 scene.add(camera)
 
-/**
- * LOOKAT
- */
-// camera.lookAt(mesh.position)
-
-
-
-/**
- * Renderer
- */
+// Renderer
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+
+
+// Time
+let time = Date.now()
+
+// Clock
+const clock = new THREE.Clock()
+
+// GSAP
+gsap.to(mesh.position, {
+    duration: 1,
+    delay: 1,
+    x: 2
+})
+
+// Animation 
+const tick = () => {
+
+    
+    // We have to adapt framerate regardless user's computer framerate
+    /**
+     * SOLUTION 1
+     */
+    // Time
+    // const currentTime = Date.now()
+    // const deltaTime = currentTime - time
+    // time = currentTime
+
+    /**
+     * SOLUTION 2
+     */
+    // CLOCK
+    // const elapsedTime = clock.getElapsedTime()
+    
+    // compatre current time to previous timestamp
+    // Update object
+    // mesh.position.y = Math.sin(elapsedTime)
+    // mesh.position.x = Math.cos(elapsedTime)
+
+    // Render
+    renderer.render(scene, camera)
+
+    window.requestAnimationFrame(tick)
+}
+
+tick()
