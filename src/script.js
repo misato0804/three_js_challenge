1c0f156addb279275cb7dd67508c037d9dc770c9
Misato Tanno <breitmeer.0804@gmail.com>
2024-07-21 12:26:01 -0600

    Added particles

--- src/script.js
+++ src/script.js
@@ -1,4 +1,12 @@
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import GUI from 'lil-gui'
+
+/**
+ * Base
+ */
+// Debug
+const gui = new GUI()
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -7,27 +15,78 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 /**
- * Object
+ * Textures
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+const textureLoader = new THREE.TextureLoader()
+const particleTexture = textureLoader.load('/textures/particles/2.png')
+
+/**
+ * Particles
+ */
+const particleGeometry = new THREE.BufferGeometry()
+const count = 50
+const color = new Float32Array(count * 3)
+const positions = new Float32Array(count * 3)
+for(let i = 0; i < count; i++) {
+    positions[i] = (Math.random() -0.5) * 10
+    color[i] = Math.random()
+}
+particleGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
+particleGeometry.setAttribute('color', new THREE.BufferAttribute(color, 3))
+
+
+const particlesMaterials = new THREE.PointsMaterial({
+    size: 0.1,
+    sizeAttenuation: true,
+    vertexColors: true,
+    color: 'pink',
+    map: particleTexture,
+    transparent: true,
+    alphaMap: particleTexture,
+    // alphaTest: 0.001,
+    // depthTest: false,
+    depthWrite: false,
+    blending: THREE.AdditiveBlending
+
+})
+
+const particles = new THREE.Points(particleGeometry, particlesMaterials)
+scene.add(particles)
 
 /**
  * Sizes
  */
 const sizes = {
-    width: 800,
-    height: 600
+    width: window.innerWidth,
+    height: window.innerHeight
 }
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
 
 /**
  * Camera
  */
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
 camera.position.z = 3
 scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
 
 /**
  * Renderer
@@ -36,4 +95,33 @@ const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update particle
+    for (let i = 0; i < count; i ++) {
+        const i3 = i * 3
+        const x = particleGeometry.attributes.position.array[i3]
+        particleGeometry.attributes.position.array[i3 + 1] = Math.sin(elapsedTime + x)
+    }
+    particleGeometry.attributes.position.needsUpdate = true
+
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
