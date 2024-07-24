ff8fc64703c886fcf72f542496857730ed395852
Misato Tanno <breitmeer.0804@gmail.com>
2024-07-24 12:29:31 -0600

    Added realistic render

--- src/script.js
+++ src/script.js
@@ -1,15 +1,6 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import GUI from 'lil-gui'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
-import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js'
-
-/**
- * Loaders
- */
-const gltfLoader = new GLTFLoader()
-const rgbeLoader = new RGBELoader()
-const textureLoader = new THREE.TextureLoader()
 
 /**
  * Base
@@ -23,128 +14,23 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
+/**
+ * Textures
+ */
+const textureLoader = new THREE.TextureLoader()
 
 /**
- * Update all materials
+ * Test mesh
  */
-const updateAllMaterials = () =>
-{
-    scene.traverse((child) =>
-    {
-        if(child.isMesh && child.material.isMeshStandardMaterial)
-        {
-            // Activate shadow here
-            child.castShadow = true
-            child.receiveShadow = true
+// Geometry
+const geometry = new THREE.PlaneGeometry(1, 1, 32, 32)
 
-        }
-    })
-}
+// Material
+const material = new THREE.MeshBasicMaterial()
 
-/**
- * Environment map
- */
-// Intensity
-scene.environmentIntensity = 1
-gui
-    .add(scene, 'environmentIntensity')
-    .min(0)
-    .max(10)
-    .step(0.001)
-
-// HDR (RGBE) equirectangular
-rgbeLoader.load('/environmentMaps/0/2k.hdr', (environmentMap) =>
-{
-    environmentMap.mapping = THREE.EquirectangularReflectionMapping
-
-    scene.background = environmentMap
-    scene.environment = environmentMap
-})
-
-
-/**
- * Directional Light
- */
-const directionalLight = new THREE.DirectionalLight('#ffffff', 6);
-directionalLight.position.set(-4, 6.5, 2.5);
-scene.add(directionalLight);
-
-gui.add(directionalLight, 'intensity').min(0).max(10).step(0.001).name('lightIntensity');
-gui.add(directionalLight.position, 'x').min(-10).max(10).step(0.001).name('lightX');
-gui.add(directionalLight.position, 'y').min(-10).max(10).step(0.001).name('lightY');
-gui.add(directionalLight.position, 'z').min(-10).max(10).step(0.001).name('lightZ');
-
-directionalLight.castShadow = true
-directionalLight.shadow.camera.far = 15
-directionalLight.shadow.mapSize.set(1024, 1024)
-
-gui.add(directionalLight, 'castShadow')
-
-//Helper
-const directionalLightHElper = new THREE.CameraHelper(directionalLight.shadow.camera)
-scene.add(directionalLightHElper)
-
-// Target
-directionalLight.target.position.set(0, 8, 0)
-directionalLight.target.updateMatrixWorld()
-
-/**
- * Models
- */
-// Helmet
-gltfLoader.load(
-    '/models/FlightHelmet/glTF/FlightHelmet.gltf',
-    (gltf) =>
-    {
-        gltf.scene.scale.set(10, 10, 10)
-        scene.add(gltf.scene)
-
-        updateAllMaterials()
-    }
-)
-
-// Floor
-const floorColorTexture = textureLoader.load('/textures/wood_cabinet_worn_long/wood_cabinet_worn_long_diff_1k.jpg');
-floorColorTexture.colorSpace = THREE.SRGBColorSpace;
-
-const floorNormalTexture = textureLoader.load('/textures/wood_cabinet_worn_long/wood_cabinet_worn_long_nor_gl_1k.png');
-const floorAORoughnessMetalnessTexture = textureLoader.load('/textures/wood_cabinet_worn_long/wood_cabinet_worn_long_arm_1k.jpg');
-
-const floor = new THREE.Mesh(
-    new THREE.PlaneGeometry(8, 8),
-    new THREE.MeshStandardMaterial({
-        map: floorColorTexture,
-        normalMap: floorNormalTexture,
-        aoMap: floorAORoughnessMetalnessTexture,
-        roughnessMap: floorAORoughnessMetalnessTexture,
-        metalnessMap: floorAORoughnessMetalnessTexture
-    })
-);
-floor.rotation.x = - Math.PI * 0.5;
-floor.receiveShadow = true;
-scene.add(floor);
-
-const wallColorTexture = textureLoader.load('/textures/wood_cabinet_worn_long/wood_cabinet_worn_long_diff_1k.jpg');
-wallColorTexture.colorSpace = THREE.SRGBColorSpace;
-
-const wallNormalTexture = textureLoader.load('/textures/wood_cabinet_worn_long/wood_cabinet_worn_long_nor_gl_1k.png');
-const wallAORoughnessMetalnessTexture = textureLoader.load('/textures/wood_cabinet_worn_long/wood_cabinet_worn_long_arm_1k.jpg');
-
-const wall = new THREE.Mesh(
-    new THREE.PlaneGeometry(8, 8),
-    new THREE.MeshStandardMaterial({
-        map: wallColorTexture,
-        normalMap: wallNormalTexture,
-        aoMap: wallAORoughnessMetalnessTexture,
-        roughnessMap: wallAORoughnessMetalnessTexture,
-        metalnessMap: wallAORoughnessMetalnessTexture
-    })
-);
-wall.position.y = 4;
-wall.position.z = -4;
-wall.receiveShadow = true;
-scene.add(wall);
-
+// Mesh
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
 
 /**
  * Sizes
@@ -174,47 +60,31 @@ window.addEventListener('resize', () =>
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.set(4, 5, 4)
+camera.position.set(0.25, - 0.25, 1)
 scene.add(camera)
 
 // Controls
 const controls = new OrbitControls(camera, canvas)
-controls.target.y = 3.5
 controls.enableDamping = true
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas,
-    antialias: true
+    canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-// Shadoes
-renderer.shadowMap.enabled = true
-renderer.shadowMap.type = THREE.PCFShadowMap
-
-
-// Tone mapping
-renderer.toneMapping = THREE.ACESFilmicToneMapping
-renderer.toneMappingExposure = 1
-
-gui.add(renderer, 'toneMapping', {
-    No: THREE.NoToneMapping,
-    Linear: THREE.LinearToneMapping,
-    Reinhard: THREE.ReinhardToneMapping,
-    Cineon: THREE.CineonToneMapping,
-    ACESFFilmic: THREE.ACESFilmicToneMapping,
-})
-gui.add(renderer, 'toneMappingExposure').min(0).max(10).step(0.01)
-
 /**
  * Animate
  */
+const clock = new THREE.Clock()
+
 const tick = () =>
 {
+    const elapsedTime = clock.getElapsedTime()
+
     // Update controls
     controls.update()
 
